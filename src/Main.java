976c38adaa3474713a992979ed034143f2b71410
ommatr <100462191+ommatr@users.noreply.github.com>
2022-03-15 09:39:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     
     public static void main(String[] args) {
+        
         try {
             switch (args[0].toLowerCase()) {
                 case "v0" -> System.out.println(V0.func(Double.parseDouble(args[1])));
