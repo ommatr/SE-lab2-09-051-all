9bdbec6040ad58b08e26e372719300caf70b5720
wlol4n <NOKosolapov@stud.kpfu.ru>
2022-04-04 13:51:19 +0300

    add v6

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
             switch (args[0].toLowerCase()) {
                 case "v0" -> System.out.println(V0.func(Double.parseDouble(args[1])));
                 case "v2" -> System.out.println(V2.compute(Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3]),Double.parseDouble(args[4])));
+                case "v6" -> System.out.println(V6.function(Double.parseDouble(args[1])));
                 case "v10" -> System.out.println(V10.compute(Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3]);
                 case "exit" -> {
                     System.out.println("Done");
