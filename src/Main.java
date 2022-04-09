46cf12e5c1a4a5f6162d68f4af80157af5fa3116
Zumk-zzz <Zumk.zzz@gmail.com>
2022-04-09 18:01:06 +0300

    develop

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
                 case "v0" -> System.out.println(V0.func(Double.parseDouble(args[1])));
                 case "V15" -> System.out.println(V15.compute(Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3]),Double.parseDouble(args[4])));
                 case "exit" -> {
-                    System.out.println("Done");
+                    System.out.println("Vipolnil");
                     return;
                 }
             }
         } catch (Exception e) {
-            System.out.println("Что-то пошло не так");
+            System.out.println("Oshibka(");
         }
     }
 }
