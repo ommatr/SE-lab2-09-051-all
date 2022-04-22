b033f456f62d907aa156f771742dbefe3b8d10b3
kanisha256 <103432178+kanisha256@users.noreply.github.com>
2022-04-23 02:50:49 +0500

    =)v9

---
+++ src/V9.java
@@ -0,0 +1,6 @@
+public class V9 {
+    public static double urav(double elem1, double elem2, double elem3) {
+        double result = Math.abs(Math.sin(elem1))*Math.cos(elem1/2.)+Math.sqrt(Math.pow(elem2,2)+Math.pow(elem3,2));
+        return result;
+    }
+}
