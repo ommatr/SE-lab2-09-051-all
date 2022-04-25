d4e7474ccaa86294126539b99564208072cfb445
kanisha256 <103432178+kanisha256@users.noreply.github.com>
2022-04-26 01:36:26 +0500

    +1

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,25 @@
-package src;public class Client {
+package src;
+
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket = new Socket("localhost",4004);
+        OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+        BufferedReader reader= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String message =reader.readLine();
+        System.out.println(message);
+        String arr = in.readLine();
+        out.write(arr + "\n");
+        out.flush();
+        String newMessage=reader.readLine();
+        System.out.println(newMessage);
+
+        out.close();
+        reader.close();
+        clientSocket.close();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,36 @@
-package src;public class Server {
+package src;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4004);
+        System.out.println("Сервер запущен!");
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            BufferedReader reader= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            writer.write("Введите вариант, угол , первую и вторую переменные\n");
+            writer.flush();
+
+            String arr = reader.readLine();
+            String[] arrs=arr.split(" ");
+            Double result=0.0;
+            if (arrs[0].equals("v9") || arrs[0].equals("V9")) {
+                result = V9.urav(Double.valueOf(arrs[1]), Double.valueOf(arrs[2]), Double.valueOf(arrs[3]));
+                System.out.println("Вариант = "+arrs[0] + "\n" + "alpha = "+arrs[1] + "\n" + "a = "+arrs[2]+ "\n" +"b = "+arrs[3] + "\n");
+                System.out.println("Рассчитано: Результат = "+result);
+                writer.write("Result = " + result + "\n");
+                writer.flush();
+            } else {
+                System.out.println("Смените вариант");
+                System.out.println("Ошибка принудительный аборт");
+            }
+            reader.close();
+            writer.close();
+            clientSocket.close();
+        }
+    }
 }
